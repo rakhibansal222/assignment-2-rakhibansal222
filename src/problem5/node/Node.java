9df011fda89de80230fa8103a255a849b24b315a
rakhibansal222 <50793761+rakhibansal222@users.noreply.github.com>
2020-03-28 01:54:53 -0700

    Update Node.java
    problem 5

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -5,6 +5,32 @@
  *  Time: 7:06 PM
  */
 package problem5.node;
+import problem5.student.Student;
+
 // to define node properties
 public class Node {
+    private int data;
+    private Node next;
+    private Student student;
+
+    public Node(Student student) {
+        this.student = student;
+        next = null;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
