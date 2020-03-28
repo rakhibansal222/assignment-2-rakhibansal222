d68734043947e19acfeaa3b62e9546a1d8e6547d
rakhibansal222 <50793761+rakhibansal222@users.noreply.github.com>
2020-03-28 00:56:52 -0700

    Update Node.java
    comments

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -6,5 +6,30 @@
  */
 package problem3.node;
 
+import problem5.student.Student;
+
 public class Node {
+    private Student student;
+    private Node next;
+
+    public Node(Student student) {
+        this.student = student;
+        next = null;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
