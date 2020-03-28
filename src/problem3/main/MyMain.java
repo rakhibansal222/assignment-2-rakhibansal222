1b0459e3c23c01fd61189181444ea8956778b006
rakhibansal222 <50793761+rakhibansal222@users.noreply.github.com>
2020-03-28 00:41:14 -0700

    Update MyMain.java
    problem3

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,34 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
 // executable class
 // use problem5.student.Student class to create object of student
+
+import problem3.myqueue.MyPriorityQueue;
+import problem5.student.Student;
+
 public class MyMain {
+    public static void main(String[] args) {
+        MyPriorityQueue queue = new MyPriorityQueue();
+        queue.enqueue(new Student(42,"Rakhi"));queue.display();
+        //System.out.println(queue.getSize());
+        //System.out.println(queue.peek().getStudent() + " element at front");
+        queue.enqueue(new Student(2,"Ayushi"));queue.display();
+        queue.enqueue(new Student(3,"Mansi"));queue.display();
+        queue.enqueue(new Student(5,"Muskan"));queue.display();
+        queue.enqueue(new Student(1,"Ekta"));queue.display();
+        queue.enqueue(new Student(6,"Vishal"));queue.display();
+        queue.enqueue(new Student(4,"Anya"));queue.display();
+        System.out.println(queue.poll().getStudent() + " polled");queue.display();
+        //System.out.println(queue.rear().getStudent() + " is rear");
+        System.out.println(queue.poll().getStudent() + " polled");queue.display();
+        //System.out.println(queue.rear().getStudent() + " is rear");
+        System.out.println(queue.poll().getStudent() + " polled");queue.display();
+        //System.out.println(queue.rear().getStudent() + " is rear");
+        System.out.println(queue.poll().getStudent() + " polled");queue.display();
+        //System.out.println(queue.rear().getStudent() + " is rear");
+        System.out.println("The Size Queue is = " + queue.getSize());
+
+    }
 }
